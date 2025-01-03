1f370de2379c244740f5a6412163f97b2ff9f61a
pedro amaral <amaralph1995@gmail.com>
2025-01-03 19:46:06 -0300

    create getRepost controller

--- src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,14 +1,17 @@
 package com.devsuperior.dsmeta.controllers;
 
+import com.devsuperior.dsmeta.dto.SaleDTO;
+import com.devsuperior.dsmeta.entities.Sale;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.services.SaleService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -24,9 +27,11 @@ public class SaleController {
 	}
 
 	@GetMapping(value = "/report")
-	public ResponseEntity<?> getReport() {
-		// TODO
-		return null;
+	public ResponseEntity<Page<SaleDTO>> getReport(@RequestParam(value = "maxDate", required = false) String maxDate,
+												   @RequestParam(value = "minDate", required = false) String minDate,
+												   @RequestParam(value = "name", defaultValue = "") String name,
+												   Pageable pageable) {
+		return ResponseEntity.ok(service.searchReports(maxDate, minDate, name, pageable));
 	}
 
 	@GetMapping(value = "/summary")
