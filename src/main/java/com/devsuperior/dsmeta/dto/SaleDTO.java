32e7db3f376d5fd73abd975ce6aad8667fb6e697
pedro amaral <amaralph1995@gmail.com>
2025-01-03 19:46:29 -0300

    create saleDTO

--- src/main/java/com/devsuperior/dsmeta/dto/SaleDTO.java
+++ src/main/java/com/devsuperior/dsmeta/dto/SaleDTO.java
@@ -1,2 +1,65 @@
-package com.devsuperior.dsmeta.dto;public class SaleDTO {
+package com.devsuperior.dsmeta.dto;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.entities.Seller;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.time.LocalDate;
+
+public class SaleDTO {
+    private Long id;
+    private Double amount;
+    private LocalDate date;
+
+    private String sellerName;
+
+    public SaleDTO() {
+    }
+
+    public SaleDTO(Long id, LocalDate date, Double amount, String sellerName) {
+        this.id = id;
+        this.amount = amount;
+        this.date = date;
+        this.sellerName = sellerName;
+    }
+
+    public SaleDTO(Sale entity) {
+        id = entity.getId();
+        amount = entity.getAmount();
+        date = entity.getDate();
+        sellerName = entity.getSeller().getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
 }
