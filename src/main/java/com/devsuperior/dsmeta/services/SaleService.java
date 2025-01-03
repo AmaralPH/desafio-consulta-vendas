2526a1532f95aac55857b46c08b3fcad11973876
pedro amaral <amaralph1995@gmail.com>
2025-01-03 19:47:44 -0300

    create searchReports logic

--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,13 +1,23 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import com.devsuperior.dsmeta.dto.SaleDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
 public class SaleService {
@@ -19,5 +29,25 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public Page<SaleDTO> searchReports(String maxDate, String minDate, String name, Pageable pageable) {
+		LocalDate endDate;
+		LocalDate startDate;
+
+		if (maxDate != null) {
+			endDate = LocalDate.parse(maxDate);
+		} else {
+			endDate = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		}
+
+		if (minDate != null) {
+			startDate = LocalDate.parse(minDate);
+		} else {
+			startDate = endDate.minusYears(1);
+		}
+
+		return repository.findSalesByFilter(endDate, startDate, name, pageable);
+
 	}
 }
