7ae8f18370e46c47b481a680f3b30e882384560b
pedro amaral <amaralph1995@gmail.com>
2025-01-03 19:47:08 -0300

    create query for report

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,27 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.dto.SaleDTO;
+import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleDTO(s.id, s.date, s.amount, se.name) " +
+            "FROM Sale s " +
+            "JOIN s.seller se " +
+            "WHERE s.date BETWEEN :startDate AND :endDate " +
+            "AND UPPER(se.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+    Page<SaleDTO> findSalesByFilter(@Param("endDate") LocalDate endDate,
+                                    @Param("startDate") LocalDate startDate,
+                                    @Param("name") String name,
+                                    Pageable pageable);
 }
