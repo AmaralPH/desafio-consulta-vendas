6bc349a0568e57bd9998b2cfeb0a6675ea4ae9d5
pedro amaral <amaralph1995@gmail.com>
2025-01-03 20:34:48 -0300

    create query for summary

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,7 +1,7 @@
 package com.devsuperior.dsmeta.repositories;
 
-import com.devsuperior.dsmeta.dto.SaleDTO;
-import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.ReportDTO;
+import com.devsuperior.dsmeta.dto.SummaryDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,13 +15,21 @@ import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
-    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleDTO(s.id, s.date, s.amount, se.name) " +
+    @Query("SELECT new com.devsuperior.dsmeta.dto.ReportDTO(s.id, s.date, s.amount, se.name) " +
             "FROM Sale s " +
             "JOIN s.seller se " +
             "WHERE s.date BETWEEN :startDate AND :endDate " +
             "AND UPPER(se.name) LIKE UPPER(CONCAT('%', :name, '%'))")
-    Page<SaleDTO> findSalesByFilter(@Param("endDate") LocalDate endDate,
-                                    @Param("startDate") LocalDate startDate,
-                                    @Param("name") String name,
-                                    Pageable pageable);
+    Page<ReportDTO> findSalesByFilter(@Param("endDate") LocalDate endDate,
+                                      @Param("startDate") LocalDate startDate,
+                                      @Param("name") String name,
+                                      Pageable pageable);
+
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SummaryDTO(se.name, SUM(s.amount)) " +
+            "FROM Seller se " +
+            "JOIN se.sales s " +
+            "WHERE s.date BETWEEN :startDate AND :endDate " +
+            "GROUP BY se.name")
+    List<SummaryDTO> findSummaryByFilter(@Param("endDate") LocalDate endDate,
+                                         @Param("startDate") LocalDate startDate);
 }
